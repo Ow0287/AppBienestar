0a95381276d42c600be961f379a3fa5163d5beaa
deivisosorio3 <deiviseguiso@gmail.com>
2017-09-30 09:43:07 -0500

    asdfghjk

---
+++ app/src/main/java/com/misena/oscar/appbienestar/Lugar.java
@@ -0,0 +1,13 @@
+package com.misena.oscar.appbienestar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugar);
+    }
+}
