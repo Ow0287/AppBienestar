a9f2285c19ceb6027a8d3cd2c029326c98ed73ba
Ow0287 <owrivero@misena.edu.co>
2017-10-01 23:57:31 -0500

    xcvb

--- app/src/main/java/com/misena/oscar/appbienestar/Himno.java
+++ app/src/main/java/com/misena/oscar/appbienestar/Himno.java
@@ -1,69 +1,40 @@
 package com.misena.oscar.appbienestar;
 
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnPreparedListener;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+
 public class Himno extends Activity {
 
-    // Declare variables
-    ProgressDialog pDialog;
-    VideoView videoview;
 
-    // Insert your Video URL
-    String VideoURL = "https://www.youtube.com/watch?v=SDZJjLAbG_Y";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the layout from video_main.xml
         setContentView(R.layout.activity_himno);
-        // Find your VideoView in your video_main.xml layout
-        videoview = (VideoView) findViewById(R.id.VideoView);
-        // Execute StreamVideo AsyncTask
 
-        // Create a progressbar
-        pDialog = new ProgressDialog(Himno.this);
-        // Set progressbar title
-        pDialog.setTitle("Himno Oficial SENA");
-        // Set progressbar message
-        pDialog.setMessage("Cargando Himno...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(false);
-        // Show progressbar
-        pDialog.show();
+        VideoView videoView = (VideoView) findViewById(R.id.VideoView);
 
-        try {
-            // Start the MediaController
-            MediaController mediacontroller = new MediaController(
-                    Himno.this);
-            mediacontroller.setAnchorView(videoview);
-            // Get the URL from String VideoURL
-            Uri video = Uri.parse(VideoURL);
-            videoview.setMediaController(mediacontroller);
-            videoview.setVideoURI(video);
-            videoview.setVideoPath("/mnt/sdcard/video.mp4");
 
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
 
-        videoview.requestFocus();
-        videoview.setOnPreparedListener(new OnPreparedListener() {
-            // Close the progress bar and play the video
-            public void onPrepared(MediaPlayer mp) {
-                pDialog.dismiss();
-                videoview.start();
-            }
-        });
+        Uri path = Uri.parse("android.resource://com.misena.oscar.appbienestar/"
+                + R.raw.himnosena);
 
+
+
+//AMPLIACIÓN
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+
+
+
+        videoView.setVideoURI(path);
+        videoView.start();
     }
 
 }
